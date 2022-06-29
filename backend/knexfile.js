cc54dfd23d57bd5392bab963978aaa5341392d61
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2022-06-29 20:20:50 -0300

    validação cadastro de admins

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
-    client: 'pg',
-    connection: {
-      database: 'base_do_conhecimento',
-      user:     'postgres',
-      password: 'tucano3d'
-    },
+    client: 'postgres',
+    connection: db,
     pool: {
       min: 2,
       max: 10
